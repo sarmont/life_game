2a0945d373bceffda489a7ca30100fbd9ece44a2
krosser006@gmail.com <ENG23TxZFpNEP9T>
2021-12-24 08:05:28 +0300

    ADD FOR loop

--- git_project_2/main.py
+++ git_project_2/main.py
@@ -10,8 +10,6 @@ class Window(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi('UI.ui', self)
-        self.pixmap = QPixmap()
-        #self.image = QLabel(self)
         self.flag = False
         self.dx = self.frameGeometry().width()
         self.dy = self.frameGeometry().height()
@@ -20,16 +18,17 @@ class Window(QMainWindow):
     def paintEvent(self, event):
         if self.flag == True:
             painter = QPainter(self)
-            painter.drawPixmap(self.rect(), self.pixmap)
             self.circle(painter)
-
+            painter.end()
 
     def circle(self, qp):
-        self.x = random.randint(1, self.dx - 1)
-        self.y = random.randint(1, self.dy - 1)
-        qp.setBrush(QColor(255, 255, 0))
-        width = random.randint(1, min(min(abs(self.x - self.dx), abs(self.y - self.dy)), min(self.x, self.y)))
-        qp.drawEllipse(self.x - width // 2, self.y - width // 2, width, width)
+        for i in range(40):
+            self.x = random.randint(1, self.dx - 1)
+            self.y = random.randint(1, self.dy - 1)
+            qp.setBrush(QColor(255, 255, 0))
+            # qp.setPen(QColor(255, 255, 0))
+            width = random.randint(1, min(min(abs(self.x - self.dx), abs(self.y - self.dy)), min(self.x, self.y)))
+            qp.drawEllipse(self.x - width // 2, self.y - width // 2, width, width)
 
     def paint(self):
         self.flag = True
